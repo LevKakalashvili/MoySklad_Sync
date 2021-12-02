9774706661205f6365b3fd0dbac17e6b634296ea
Lev <kakalashvililev@yandex.ru>
2021-12-02 10:46:00 +0300

    Добавил фал настроек логгеров

---
+++ logger_config.py
@@ -0,0 +1,57 @@
+"""
+Модуль конфигурирования логгеров
+за основу взято https://gist.github.com/chud0/bab1ea18ccba89ab6d1fa4da8a64a249
+"""
+
+DEBUG = True
+
+LOGGING_CONF = {
+    "disable_existing_loggers": True,
+    "version": 1,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)-8s %(asctime)s [%(filename)s:%(lineno)d] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+        "brief": {
+            "format": "%(levelname)-8s %(asctime)s %(name)-16s %(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "formatter": "verbose",
+            "filename": "app.log",
+            "encoding": "UTF-8",
+            "mode": "w"
+        },
+    },
+    "loggers": {
+        "main_logger": {
+            "level": "DEBUG" if DEBUG else "INFO",
+            "handlers": ["file"],
+        },
+        "tbot": {
+            "level": "DEBUG" if DEBUG else "INFO",
+            "handlers": ["file"],
+        },
+       "moysklad": {
+           "level": "DEBUG" if DEBUG else "INFO",
+            "handlers": ["file"],
+        },
+        "google": {
+           "level": "DEBUG" if DEBUG else "INFO",
+            "handlers": ["file"],
+        },
+        "utils": {
+           "level": "DEBUG" if DEBUG else "INFO",
+            "handlers": ["file"],
+        },
+    },
+}
