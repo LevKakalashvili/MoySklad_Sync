06fa10591c72fab69df71f17c22914a3205f2b94
Lev <kakalashvililev@yandex.ru>
2021-12-09 22:10:10 +0300

    Исправил возвращаемы значение. Написал причину использования pandas

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -3,13 +3,13 @@ import pandas as pd
 import datetime
 
 
-
 def remove_file(file_name: str):
     try:
         if file_name != '' and os.path.isfile(file_name):
             os.remove(file_name)
     except Exception as error:
         pass
+
 
 def save_to_excel(file_name: str, data: list, add_date=True) -> str:
     """
@@ -19,28 +19,34 @@ def save_to_excel(file_name: str, data: list, add_date=True) -> str:
     :param add_date:
     :return: Имя файла
     """
+
     if not data:
         return ''
-    try:
-        if add_date:
-            new_file_name = file_name + '_' + str(datetime.datetime.now().date() - datetime.timedelta(days=1)) + '.xlsx'
-            remove_file_name = file_name + '_' + \
-                               str(datetime.datetime.now().date() - datetime.timedelta(days=2)) + '.xlsx'
-        else:
-            remove_file_name = ''
-        # пишем данные в файл
-        df = pd.DataFrame(data)
-        df.to_excel(new_file_name, sheet_name='Списания ЕГАИС', index=False, header=False)
-        return new_file_name
 
-    except Exception as error:
-        return ''
+    if add_date:
+        new_file_name = file_name + '_' + str(datetime.datetime.now().date() - datetime.timedelta(days=1)) + '.xlsx'
+        remove_file_name = file_name + '_' + \
+                           str(datetime.datetime.now().date() - datetime.timedelta(days=2)) + '.xlsx'
+    else:
+        remove_file_name = ''
+
+    # В конечном итоге, мне нужен именно excel, т.к. продавцы которые пользуют сервис, скачивают файл из телеги,
+    # люди не далекие. Открыть Excel они могут, а вот делать доп. действия с csv будет сложно объяснить и инструкции
+    # тут не помогут.
+    # Да, ставить pandas, и делать DataFrame для этих целей не очень-то целесообразно. Но меня устроила компактность
+    # кода.
+
+    # пишем данные в файл
+
+    df = pd.DataFrame(data)
+    df.to_excel(new_file_name, sheet_name='Списания ЕГАИС', index=False, header=False)
+    return new_file_name
 
 
-def save_to_txt(file_name: str, data: list, add_date=True) -> int:
+def save_to_txt(file_name: str, data: list, add_date=True) -> bool:
     """ Сохранение в текстовый файл"""
     if not data:
-        return -1
+        return False
     try:
         if add_date:
             new_file_name = file_name + '_' + str(datetime.datetime.now().date() - datetime.timedelta(days=1)) + '.txt'
@@ -59,4 +65,4 @@ def save_to_txt(file_name: str, data: list, add_date=True) -> int:
         if remove_file_name != '' and os.path.isfile(remove_file_name):
             os.remove(remove_file_name)
     except Exception as error:
-        return -1
+        return False
