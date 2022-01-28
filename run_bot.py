09bd4a7aa8b94a9b2eaa8dfa059d916a8df17b17
Lev <kakalashvililev@yandex.ru>
2022-01-28 14:48:25 +0300

    Поправил запуск бота

--- run_bot.py
+++ run_bot.py
@@ -3,11 +3,7 @@ import os
 import tbot.tbot
 
 
-def run_bot() -> None:
-    tbot.tbot.run()
-
-
 if __name__ == '__main__':
     if os.name == 'nt':
         os.environ.setdefault('DEBUG', str(True))
-    run_bot()
+    tbot.tbot.run()
