f0883a4baba12f42321ab2b4062bee67c4238218
Lev <kakalashvililev@yandex.ru>
2022-01-29 17:03:57 +0300

    Добавил комменты

--- run_bot.py
+++ run_bot.py
@@ -1,5 +1,9 @@
+"""Модуль для запуска бота Telegram."""
 import tbot.tbot as telegram_bot
 
 
 if __name__ == '__main__':
     telegram_bot.run()
+
+# Запуск в фоне на сервере.
+# sudo python3 /var/geo/MoySklad_Sync/run_bot.py &
