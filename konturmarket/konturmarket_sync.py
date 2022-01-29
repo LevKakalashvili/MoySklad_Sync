b52ca83f55649a1ae524c1d276075c4485968b44
Lev <kakalashvililev@yandex.ru>
2022-01-29 17:02:37 +0300

    Исправил импорты

--- konturmarket/konturmarket_sync.py
+++ konturmarket/konturmarket_sync.py
@@ -1,7 +1,7 @@
 """Модуль для запуска синхронизации Контур.Маркета с БД"""
 from typing import List
-from konturmarket_class_lib import KonturMarket, GoodEGAIS
 
+from konturmarket_class_lib import KonturMarket, GoodEGAIS
 
 if __name__ == "__main__":
     kmarket = KonturMarket()
