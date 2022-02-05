e6f00b43e0a1275d5484f5d68825f97a0064e85a
Lev <kakalashvililev@yandex.ru>
2022-02-05 14:34:41 +0300

    - Добавил класс Brewery для получения короткого наименования поставщика - Дополнил класс GoodEGAIS

--- konturmarket/konturmarket_class_lib.py
+++ konturmarket/konturmarket_class_lib.py
@@ -1,19 +1,28 @@
-"""В модуле описаны классы для работы с сервисом Контур.Маркет https://market.kontur.ru/"""
+"""В модуле описаны классы для работы с сервисом Контур.Маркет https://market.kontur.ru/."""
 import json
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import privatedata.kontrurmarket_privatedata as km_pvdata
 import requests
 from pydantic import BaseModel, Field
 
-from konturmarket_urls import Url, UrlType, get_url
+from konturmarket.konturmarket_urls import Url, UrlType, get_url
 
 session = requests.Session()
 
 
+class Brewery(BaseModel):
+    """Клас описывает структуру компании производителя, продукции в соответствии с терминами ЕГАИС. Словарь
+    'producer' в JSON представлении."""
+
+    name: str = Field(alias='shortName')
+
+
 class GoodEGAIS(BaseModel):
-    """Клас описывает структуру товара, продукции в соответствии с терминами ЕГАИС"""
+    """Клас описывает структуру товара, продукции в соответствии с терминами ЕГАИС.
+    Словарь 'productInfo' в JSON представлении."""
+
     # ЕГАИС наименование
     name: str = Field(alias='fullName')
 
@@ -21,21 +30,58 @@ class GoodEGAIS(BaseModel):
     # коде меньше 19-ти, то вперед дописываются нули. Это при строковом представлении
     alco_code: str = Field(alias='egaisCode')
 
-    def to_tuple(self) -> Tuple[str, str]:
-        """Метод возвращает кортеж вида (ЕГАИС_КОД, ЕГАИС_НАИМЕНОВАНИЕ)."""
-        return (self.alco_code, self.name)
+    # Емкость тары. Необязательный параметр. Отсутствие этого параметра говорит о том, что товар разливной
+    capacity: Optional[float] = None
+
+    # Описание производителя
+    brewery: Brewery = Field(alias='producer')
+
+    def to_tuple(self) -> Tuple[str, str, str]:
+        """Метод возвращает кортеж вида (ЕГАИС_НАИМЕНОВАНИЕ, ЕГАИС_КОД)."""
+        return self.name, self.alco_code, self.brewery.name
+
+    def get_description(self):
+        return (
+            f'Пивоварня: {self.brewery.name}  '
+            f'Наименование: {self.name}  '
+            f'Код: {self.alco_code}  '
+        )
 
 
 @dataclass()
 class KonturMarket:
-    """Класс описывает работу с сервисом Контур.Маркет https://market.kontur.ru/"""
+    """Класс описывает работу с сервисом Контур.Маркет https://market.kontur.ru/."""
+
+    # Переменная устанавливается в True, в случае успешного логина в сервисе
+    connection_OK: bool = False
 
     @staticmethod
-    def login() -> bool:
+    def get_egais_assortment() -> List[GoodEGAIS]:
+        """Метод возвращает список инстансов GoodEGAIS, полученных из сервиса."""
+
+        url: Url = get_url(UrlType.egais_assortment)
+        response = session.get(url.url)
+
+        goods = dict(response.json()).get('list')
+        # Если получили успешный ответ и есть список товаров
+        if response.ok and goods:
+            goods_list: List[GoodEGAIS]
+            # Проходим по всему списку товаров, наименований.
+            for good in goods:
+                # Получаем словарь с информацией о товаре
+                goods_list.append(GoodEGAIS(**good['productInfo']))
+
+            # Сортировка по названию пивоварни
+            goods_list = sorted(goods_list, key=lambda element: element.brewery.name)
+            return goods_list
+        else:
+            return []
+
+    def login(self) -> bool:
         auth_data = {
             'Login': km_pvdata.USER,
             'Password': km_pvdata.PASSWORD,
-            'Remember': False
+            'Remember': False,
         }
         # auth_data = '{"Login":"kakalashvililev@yandex.ru","Password":"340354Lev!","Remember":false}'
         # Пытаемся залогиниться на сайте
@@ -44,24 +90,13 @@ class KonturMarket:
             url=url.url,
             data=json.dumps(auth_data),
             headers=url.headers,
-            cookies=url.cookies
+            cookies=url.cookies,
         )
+        self.connection_OK = response.ok
+
         return response.ok
 
-    @staticmethod
-    def get_egais_assortment() -> List[GoodEGAIS]:
-        goods_list: List[GoodEGAIS] = list()
 
-        url: Url = get_url(UrlType.egais_assortment)
-        response = session.get(url.url)
-
-        goods = dict(response.json()).get('list')
-        # Если получили успешный ответ и есть список товаров
-        if response.ok and goods:
-            # Проходим по всему списку товаров, наименований.
-            for good in goods:
-                # Получаем словарь с информацией о товаре
-                goods_list.append(GoodEGAIS(**good['productInfo']))
-            return goods_list
-        else:
-            return []
+# Создаем инстанс сервиса
+kmarket = KonturMarket()
+kmarket.login()
