35c3dda650203b64e83a85ca5c5384054380069e
Lev <kakalashvililev@yandex.ru>
2022-02-05 14:36:53 +0300

    - Добавил метод отправки списка ЕГАИС в GoogleSheets

--- run_by_schedule.py
+++ run_by_schedule.py
@@ -4,4 +4,8 @@ import privatedata.tbot_privatedata as pvd_telebot
 import utils.service as service
 
 if __name__ == '__main__':
+    # Отправка заполненного файла с продажами за сегодня в телеграм чат
     service.send_sales_file_to_telegram(pvd_telebot.TELEGRAM_GEO_CHAT_ID)
+
+    # Обновление таблицы GoogleSheets новым списком ЕГАИС наименований
+    service.update_goooglesheets_egais_assortment(pvd_telebot.TELEGRAM_GEO_CHAT_ID)
