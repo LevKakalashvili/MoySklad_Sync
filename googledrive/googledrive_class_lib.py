cd0805859275fcb87670fcf688c9677e3b0ea4f7
Lev <kakalashvililev@yandex.ru>
2022-02-05 14:32:15 +0300

    - Переделал GoogleSheets в dataclass - Изменил GoogleSheets - Добавил метод send_data

--- googledrive/googledrive_class_lib.py
+++ googledrive/googledrive_class_lib.py
@@ -1,27 +1,33 @@
 """Модуль для работы с Google Sheets"""
-import logging
-import logging.config
 import os
+from dataclasses import dataclass
 from typing import Any, List
 
 import googleapiclient.discovery  # pip install google-api-python-client
 import httplib2  # pip install httplib2
+from googleapiclient.errors import HttpError
 from oauth2client.service_account import ServiceAccountCredentials
 
 import googledrive.googlesheets_vars as gs_vars
+
+import logging.config
+
 import logger_config
+
 
 logging.config.dictConfig(logger_config.LOGGING_CONF)
 # Логгер для GoogleDrive
 gs_logger = logging.getLogger('google')
 
 
+@dataclass
 class GoogleSheets:
     """Класс для чтения данных из Google Sheets"""
 
-    def __init__(self) -> None:
-        self.service: Any = None  # сервисный объект для работы с Google API
-        self.get_access()
+    # Cервисный объект для работы с Google API.
+    service: Any = None
+    # Переменная устанавливается в True, в случае успешного логина в сервисе.
+    connection_OK: bool = False
 
     def get_access(self) -> bool:
         """"Метод получения доступа сервисного объекта Google API
@@ -38,13 +44,15 @@ class GoogleSheets:
         try:
             self.service = googleapiclient.discovery.build('sheets', 'v4', http=http_auth)
             gs_logger.debug('Получили доступ к Google API')
+            self.connection_OK = True
+
             return True
         except googleapiclient.discovery.MutualTLSChannelError as error:
             gs_logger.error(f'Не удалось создать сервисный объект для работы с Google API: {error.args[0]}')
             return False
 
     def get_data(self, spreadsheets_id: str, list_name: str, list_range: str) -> List[List[str]]:
-        """
+        """Метод получения данных из таблицы GoogleSheets
         :param spreadsheets_id: id таблицы в Google Sheets
         :param list_name: текстовое имя листа
         :param list_range: запрашиваемый диапазон A1:H100
@@ -68,6 +76,44 @@ class GoogleSheets:
         while not values[i]:
             i += 1
         return values[i:]
+
+    def send_data(self, data: List[Any], spreadsheets_id: str, list_name: str, list_range: str) -> bool:
+        """Метод записи данных в таблицу GoogleSheets.
+        :param data: Данные для записи.
+        :param spreadsheets_id: id таблицы в Google Sheets
+        :param list_name: Текстовое имя листа.
+        :param list_range: Запрашиваемый диапазон A1:H100.
+        :return: Возвращает True в случае удачной записи, False в случае ошибки.
+        """
+
+        # В начале очищаем диапазон.
+        # https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/clear?hl=ru
+        self.service.spreadsheets().values().clear(spreadsheetId=spreadsheets_id,
+                                                   range=f'{list_name}!{list_range}',
+                                                   body={}).execute()
+        # Записываем данные
+        try:
+            self.service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheets_id,
+                                                         body={
+                                                             "valueInputOption": "USER_ENTERED",
+                                                             "data":
+                                                                 [
+                                                                     {
+                                                                         "range": f'{list_name}!{list_range}',
+                                                                         "majorDimension": "ROWS",
+                                                                         "values": data
+                                                                     }
+                                                                 ]
+                                                         }).execute()
+            return True
+        except HttpError:
+            return False
+
+
+
+# Создаем инстанс GoogleSheets
+googlesheets = GoogleSheets()
+googlesheets.get_access()
 
 if __name__ == '__main__':
     gs = GoogleSheets()
